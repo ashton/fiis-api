8d97576ca4bdcdc64ab701e51c89343c433093dd
Matheus <matheus.ashton@nubank.com.br>
2021-06-10 16:31:24 -0300

    improving funds adapters

--- src/fiis_api/adapters/funds.clj
+++ src/fiis_api/adapters/funds.clj
@@ -1,8 +1,14 @@
 (ns fiis-api.adapters.funds
-  (:require [schema.core :as s]
+  (:require [clojure.set :refer [rename-keys]]
+            [fiis-api.models.funds :as schema.model]
             [fiis-api.schemata.out.funds :as schema.out]
-            [fiis-api.models.funds :as schema.model]))
+            [fiis-api.schemata.postgres.funds :as schema.db]
+            [schema.core :as s]))
 
 (s/defn model->external :- schema.out/Fund
-  [fund] :- schema.model/Fund
-  (assoc fund :quota_amount (:quota-amount fund)))
+  [fund :- schema.model/Fund]
+  (rename-keys fund {:quota-amount :quota_amount}))
+
+(s/defn db->model :- schema.model/Fund
+  [fund :- schema.db/Fund]
+  (rename-keys fund {:quota_amount :quota-amount}))
